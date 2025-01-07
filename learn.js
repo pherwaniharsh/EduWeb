0b554792088759015dd9bca79fd8ff082a13a442
pherwaniharsh <rmpherwani.hp@protonmail.com>
2025-01-07 17:46:09 +0530

    Added .vscode settings + minor refactoring

--- learn.js
+++ learn.js
@@ -3,14 +3,14 @@ let navigationBar = document.querySelector("nav");
 let cancelButton = document.querySelector(".hamCancel");
 
 
-hamburgerMenu.addEventListener('click', function(){
-    
+hamburgerMenu.addEventListener('click', function () {
+
     navigationBar.classList.add("navDisplay");
     cancelButton.classList.add("navDisplay");
 });
 
-cancelButton.addEventListener("click", function(){
-    
+cancelButton.addEventListener("click", function () {
+
     navigationBar.classList.remove("navDisplay");
     cancelButton.classList.remove("navDisplay");
 });
